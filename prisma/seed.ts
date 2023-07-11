626365daa5a959177744198250e0a710babf4bd1
DuyLX <duylxbk57@gmail.com>
2023-07-11 21:23:29 +0700

    :construction_worker: Add more movie seeding data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,44 +2,50 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient()
 
 async function main() {
-  // const quotes = [
-  //   {
-  //     quote: "Toto, I've got a feeling we're not in Kansas anymore.",
-  //     saidBy: 'Dorothy Gale',
-  //     movie: 'The Wizard of Oz',
-  //   },
-  //   {
-  //     quote: "You're gonna need a bigger boat.",
-  //     saidBy: 'Martin Brody',
-  //     movie: 'Jaws',
-  //   },
-  //   {
-  //     quote: 'May the Force be with you.',
-  //     saidBy: 'Han Solo',
-  //     movie: 'Star Wars',
-  //   },
-  //   {
-  //     quote: 'I have always depended on the kindness of strangers.',
-  //     saidBy: 'Blanche DuBois',
-  //     movie: 'A Streetcar Named Desire',
-  //   },
-  // ]
-  const movie = await prisma.movie.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      name: 'Black And White',
-      quotes: {
-        create: {
-          quote: 'This is good movie!',
-          saidBy: 'Duyhihi'
-        }
-      }
-    }
-  })
+  const quotes = [
+    {
+      quote: "Toto, I've got a feeling we're not in Kansas anymore.",
+      saidBy: 'Dorothy Gale',
+      movie: 'The Wizard of Oz',
+      movieId: 1,
+    },
+    {
+      quote: "You're gonna need a bigger boat.",
+      saidBy: 'Martin Brody',
+      movie: 'Jaws',
+      movieId: 2,
+    },
+    {
+      quote: 'May the Force be with you.',
+      saidBy: 'Han Solo',
+      movie: 'Star Wars',
+      movieId: 3,
+    },
+    {
+      quote: 'I have always depended on the kindness of strangers.',
+      saidBy: 'Blanche DuBois',
+      movie: 'A Streetcar Named Desire',
+      movieId: 4,
+    },
+  ]
 
-  console.log({movie})
-  
+  for (const { movieId, quote, saidBy, movie: movieName } of quotes) {
+    const movie = await prisma.movie.upsert({
+      where: { id: movieId },
+      update: {},
+      create: {
+        name: movieName,
+        quotes: {
+          create: {
+            quote: quote,
+            saidBy: saidBy,
+          },
+        },
+      },
+    });
+
+    console.log('created movie: ', {movie})
+  }  
 }
 
 main()
