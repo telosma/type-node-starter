d3ff463093cd14ba1f9cdd03536cc14f4deb8890
hoang bac <hoangvietbac090699@gmail.com>
2023-07-14 01:38:08 +0700

    ✨ Add CRUD for the movies and quotes

--- src/controllers/QuoteController.ts
+++ src/controllers/QuoteController.ts
@@ -2,10 +2,48 @@ import { Request, Response } from 'express'
 import prisma from '../database'
 
 class QuoteController {
-  static index = async(req: Request, res: Response) => {
-    const quotes = await prisma.quote.findMany()
 
+  //GET quotes
+  static getQuotes = async(req: Request, res: Response) => {
+    const quotes = await prisma.quote.findMany()
     return res.send(quotes)
+  }
+
+  //POST quote
+  static postQuote = async(req: Request, res: Response) => {
+    const {quote, saidBy,movieId} = req.body
+    const post = await prisma.quote.create({
+      data:{
+        quote,
+        saidBy,
+        movie: {connect: { id: Number(movieId)}},
+      },
+    })
+    return res.send(post)
+  }
+
+  //UPDATE quote
+  static updateQuote =  async(req: Request, res: Response) => {
+    const {id} = req.params
+    const {quote} = req.body
+    const updateQuote = await prisma.quote.update({
+      where:{id: Number(id)},
+      data:{
+        quote
+      }
+    })
+    return res.send(updateQuote)
+  }
+
+  //DELETE quote
+  static delQuote = async(req: Request, res: Response) => {
+    const {id} = req.params
+    const del = await prisma.quote.delete({
+      where:{
+        id: Number(id)
+      }
+    })
+    return res.send(del)
   }
 }
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,21 @@
 import { Router, Request, Response } from 'express'
 import QuoteController from '@root/controllers/QuoteController'
-
+import MoviesController from '@root/controllers/MoviesController'
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => { return res.send('Hello world!') })
 
-router.get('/quotes', QuoteController.index)
+
+//Movies
+router.get('/movies',MoviesController.getMovies)
+router.post('/movies',MoviesController.postMovie)
+router.patch('/movies/:id',MoviesController.updateMovie)
+router.delete('/movies/:id',MoviesController.delMovie)
+
+//Quotes
+router.get('/quotes', QuoteController.getQuotes)
+router.post('/quotes',QuoteController.postQuote)
+router.patch('/quotes/:id',QuoteController.updateQuote)
+router.delete('/quotes/:id',QuoteController.delQuote)
 
 export default router
